162f4bb8a314b588e85523b7775c6dc07e7a80b1
lukas175 <79913864+lukas175@users.noreply.github.com>
2022-10-09 15:31:00 +0200

    asd

--- server/server.go
+++ server/server.go
@@ -1,7 +1,28 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/Lukski175/GO-Exercise5/time"
+)
 
 func main() {
-	fmt.Printf("something")
+	fmt.Println("Input port to listen on...")
+	var port string
+	fmt.Scan(&port)
+
+	// Create listener tcp on port 9080
+	fmt.Printf("Listening on port %s", port)
+	list, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", port, err)
+	}
+	grpcServer := grpc.NewServer()
+	time.RegisterTimeServiceServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
 }
